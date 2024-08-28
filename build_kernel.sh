2a4e48dff53ecabd8ab83f129af000c99b170ad7
SUFandom <bluepearl394@gmail.com>
2024-08-28 09:50:09 +0000

    initial commit

---
+++ build_kernel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+export PLATFORM_VERSION=13
+export ANDROID_MAJOR_VERSION=t
+export ARCH=arm64
+
+
+make clean && make mrproper
+make menuconfig
+make ARCH=arm64 exynos850-a12snsxx_defconfig
+make ARCH=arm64 -j64
